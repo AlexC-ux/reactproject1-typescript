c9db94a476fe63ab502da061e0aed8adb1250664
AlexC-ux <eunv2004@gmail.com>
2022-07-03 19:47:17 +0300

    new encoding - utf8

--- src/prezi.tsx
+++ src/prezi.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
+п»їimport React from 'react';
 
 function ReactComponent() {
     return (
         <div>
             <div className="container px-4 py-5" id="custom-cards">
-                <h2 className="pb-2 border-bottom">Конкурс презентаций
+                <h2 className="pb-2 border-bottom">РљРѕРЅРєСѓСЂСЃ РїСЂРµР·РµРЅС‚Р°С†РёР№
                     <span className="badge bg-primary rounded-pill">NEW</span></h2>
 
                 <div className="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
                     <div className="col">
                         <div className="card card-cover h-100 overflow-hidden text-white bg-dark rounded-4 shadow-lg" style={{ backgroundImage: "url('/images/banners/banner-5.svg')", backgroundSize: "cover" }}>
                             <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-                                <h2 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Выбери тему для своей презентации</h2>
+                                <h2 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Р’С‹Р±РµСЂРё С‚РµРјСѓ РґР»СЏ СЃРІРѕРµР№ РїСЂРµР·РµРЅС‚Р°С†РёРё</h2>
 
                             </div>
                         </div>
@@ -20,7 +20,7 @@ function ReactComponent() {
                     <div className="col">
                         <div className="card card-cover h-100 overflow-hidden text-white bg-dark rounded-4 shadow-lg" style={{ backgroundImage: "url('/images/banners/banner-6.svg')", backgroundSize: "cover" }}>
                             <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-                                <h2 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Подготовь презентацию и отправь</h2>
+                                <h2 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">РџРѕРґРіРѕС‚РѕРІСЊ РїСЂРµР·РµРЅС‚Р°С†РёСЋ Рё РѕС‚РїСЂР°РІСЊ</h2>
 
                             </div>
                         </div>
@@ -29,7 +29,7 @@ function ReactComponent() {
                     <div className="col">
                         <div className="card card-cover h-100 overflow-hidden text-white bg-dark rounded-4 shadow-lg" style={{ backgroundImage: "url('/images/banners/banner-7.svg')", backgroundSize: "cover" }}>
                             <div className="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
-                                <h2 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Ожидай результатов, дата будет объявлена позже</h2>
+                                <h2 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">РћР¶РёРґР°Р№ СЂРµР·СѓР»СЊС‚Р°С‚РѕРІ, РґР°С‚Р° Р±СѓРґРµС‚ РѕР±СЉСЏРІР»РµРЅР° РїРѕР·Р¶Рµ</h2>
 
                             </div>
                         </div>
@@ -38,7 +38,7 @@ function ReactComponent() {
 
             </div>
             <div className="d-grid gap-2 container">
-                <a className="btn btn-lg btn-dark" type="button" href="/кабинет">Участвовать</a>
+                <a className="btn btn-lg btn-dark" type="button" href="/РєР°Р±РёРЅРµС‚">РЈС‡Р°СЃС‚РІРѕРІР°С‚СЊ</a>
             </div>
         </div>
     );

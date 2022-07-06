4b14da3ffd4a50a5193163b707472cc47e1719fb
AlexC-ux <eunv2004@gmail.com>
2022-07-06 12:27:17 +0300

    game updated

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import View from './view';
 import LogPage from "./log-in";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import TickTackToe from "./tick-tack-toe";
+import ShowTickTackToeComponent from "./ticktacktoe/tick-tack-toe";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -14,7 +14,7 @@ root.render(
         <Routes>
             <Route path="/" element={<View />} />                   { /*   Главная страница     */}
             <Route path="login" element={<LogPage />} />            { /*   Страница авторизации: /войти     */}
-            <Route path="ttt" element={<TickTackToe />}>            { /*   Страница "крестики-нолики"     */}
+            <Route path="ttt" element={<ShowTickTackToeComponent/>}>            { /*   Страница "крестики-нолики"     */}
         </Route>
     </Routes>
 </BrowserRouter>);
---
+++ src/ticktacktoe/tick-tack-toe.tsx
@@ -0,0 +1,97 @@
+﻿import { render } from '@testing-library/react';
+import { table } from 'console';
+import React, { ReactComponentElement, RefObject, useRef, useState } from 'react';
+import ReactDOM from 'react-dom';
+import ".././css/ticktacktoe.css";
+
+
+function ShowTickTackToeComponent() {
+    /**Массив результатов ходов */
+    const [cellValues, SetCellState] = useState([["", "", ""], ["", "", ""], ["", "", ""]]);
+
+    /**Счётчик ходов */
+    const [steps, UpdateSteps] = useState(0);
+
+    /**Результат игры */
+    const [gameResult, SetGameResult] = useState("");
+
+    /**
+     * Функция обновления значения конкретной ячейки массива результатов ходов
+     * @param value Присваиваемое значение
+     * @param row Номер строки
+     * @param cell Номер столбца
+     */
+    function UpdateSpecificCell(value: string, row: number, cell: number) {
+        let cellsValues: string[][] = cellValues;
+        cellsValues[row][cell] = value;
+        SetCellState(cellsValues);
+    }
+
+    /**Функция проверяет игровые поля на предмет наличия победителя */
+    function VerifyResults() {
+        let results: string[] = new Array<string>(8);
+
+        for (var v = 0; v < 3; v++) {
+            results[v] = `${cellValues[v][0]}${cellValues[v][1]}${cellValues[v][2]}`;
+            results[v+3] = `${cellValues[0][v]}${cellValues[1][v]}${cellValues[2][v]}`;
+        }
+        results[6] = `${cellValues[0][0]}${cellValues[1][1]}${cellValues[2][2]}`;
+        results[7] = `${cellValues[0][2]}${cellValues[1][1]}${cellValues[0][2]}`;
+
+        results.forEach((value) => {
+            if (value == "crossscrossscrosss") {
+                SetGameResult("Игра окончена! Победили крестики!");
+                UpdateSteps(10);
+            } else if (value == "zeroesszeroesszeroess") {
+                SetGameResult("Игра окончена! Победили нолики!");
+                UpdateSteps(10);
+            }
+        })
+    }
+
+    /**
+     * Функция получения события нажатия на ячейку (крестик/нолик)
+     * @param row Строка в которой расположена ячейка
+     * @param cell Столбец в котором расположена ячейка
+     */
+    function GetCellClickEvent(row: number, cell: number) {
+        if (steps < 9 && cellValues[row][cell] == "") {
+            if (steps % 2 > 0) {
+                UpdateSpecificCell("crosss", row, cell);
+            } else {
+                UpdateSpecificCell("zeroess", row, cell);
+            }
+            UpdateSteps(steps + 1);
+            VerifyResults();
+        }
+    }
+
+    /**
+     * Функция возвращает строку игровых полей
+     * @param row Номер строки
+     */
+    function GetRow(row: number) {
+        return (
+            <tr>
+                <td className={`celli cellil ${cellValues[row][0]}`} onClick={() => GetCellClickEvent(row, 0)} />
+                <td className={`celli cellil ${cellValues[row][1]}`} onClick={() => GetCellClickEvent(row, 1)} />
+                <td className={`celli cellil ${cellValues[row][2]}`} onClick={() => GetCellClickEvent(row, 2)} />
+            </tr>
+        );
+    }
+    
+    return (
+        <div className="container">
+            <p className="text-middle">{gameResult}</p>
+            <p className="text-middle">Всего ходов: {steps}</p>
+            <table>
+                <tbody>
+                    {GetRow(0)}{GetRow(1)}{GetRow(2)}
+                </tbody>
+            </table>
+        </div>
+        );
+}
+
+
+export default ShowTickTackToeComponent;
